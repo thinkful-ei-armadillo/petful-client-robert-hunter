078407a59a9981599d4568d4d25f319d5647f71a
Hunter Kreshock <hkreshock@gmail.com>
2019-04-12 09:17:11 -0700

    added main page and link to adoption page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import MainPage from "./routes/MainPage/MainPage";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Adopt A Friend Today!</h1>
+          <img
+            className="down_arrow"
+            alt="down arrow"
+            src="https://user-images.githubusercontent.com/45447942/56050989-301c7580-5d02-11e9-99c1-b75ebadc2e46.png"
+          />
         </header>
+        <main className="App-main">
+          <Route exact path={"/"} component={MainPage} className="MainPage" />
+        </main>
       </div>
     );
   }
